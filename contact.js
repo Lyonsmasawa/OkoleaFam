b1dfdb3362ba0534724ffb0101915c2ab311efe9
12moi <moi.shadrack@student.moringaschool.com>
2021-11-18 07:19:46 +0300

    add modified files

---
+++ contact.js
@@ -0,0 +1,20 @@
+<script>
+   var form=document.getElementById('form'){
+       var error=[]
+
+       var name=document.getElementById('Name')
+       var Email=document.getElementById('Email')
+       if(name.value== "" \\ Email.value="")
+   }
+   alert("Please enter all the details")
+   if(name.value==""){
+       error.push("Please enter the name")
+   }
+   if (Email.value=="") {
+       error.push("Please enter the Email")
+   }
+   </script>
+        
+
+
+
